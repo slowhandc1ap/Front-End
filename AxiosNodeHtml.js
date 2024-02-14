7277ee8d3ed797eb7b20b29a404e6340e3579b76
slowhandc1ap <s6406021621161@email.kmutnb.ac.th>
2024-02-14 08:37:59 +0700

    AddCss

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -8,8 +8,8 @@ var bodyParser = require('body-parser');
 
 //Base URL for the API
 //const base_url = "https://api.example.com";
-const base_url = "http://localhost:3000";
-
+//const base_url = "http://localhost:3000";
+const base_url = "http://node56998-tanakorntawee-noderest.proen.app.ruk-com.cloud"
 //Set the template engine
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
