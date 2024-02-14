cef45b51f4c7cfd2341f0090f623e6787b592ccd
slowhandc1ap <s6406021621161@email.kmutnb.ac.th>
2024-02-14 10:26:48 +0700

    path

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
+const path = require("path")
 
 //Base URL for the API
 //const base_url = "https://api.example.com";
 //const base_url = "http://localhost:3000";
 const base_url = "http://node56998-tanakorntawee-noderest.proen.app.ruk-com.cloud"
 //Set the template engine
+app.set("views",path.join(__dirname,"/public/views"))
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
